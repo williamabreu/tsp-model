d1641409a94b99e85fd21ee2e9d4ade18aea087f
williamabreu <contato@williamabreu.net>
2021-02-13 13:46:56 -0300

    Formated files with Black python formatter

--- tspmodel/graph/Edge.py
+++ tspmodel/graph/Edge.py
@@ -1,7 +1,7 @@
 from tspmodel.graph import Vertex
 
-class Edge:
 
+class Edge:
     def __init__(self, u: Vertex, v: Vertex, w: float = None) -> None:
         '''An edge "u" -> "v" with cost "w"'''
         self.__vertex_src = u
@@ -10,9 +10,9 @@ class Edge:
 
     def get_source_vertex(self) -> Vertex:
         return self.__vertex_src
-    
+
     def get_destination_vertex(self) -> Vertex:
         return self.__vertex_dst
-    
+
     def get_weight(self) -> float:
         return self.__weight
--- tspmodel/graph/Vertex.py
+++ tspmodel/graph/Vertex.py
@@ -1,8 +1,7 @@
 class Vertex:
-
     def __init__(self, id: int) -> None:
-        '''A vertex "u" from the graph'''
+        """A vertex "u" from the graph"""
         self.__id = id
-    
+
     def get_id(self):
         return self.__id
