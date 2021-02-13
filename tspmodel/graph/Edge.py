c7bed4f605692f570fef15708b5d4235ff971a49
williamabreu <contato@williamabreu.net>
2021-02-13 13:42:37 -0300

    Add Vertex and Edge models

--- tspmodel/graph/Edge.py
+++ tspmodel/graph/Edge.py
@@ -1,7 +1,18 @@
-from src.graph import Vertex
+from tspmodel.graph import Vertex
 
 class Edge:
-    '''An edge "a" -> "b" from the graph'''
 
-    def __init__(self, a: Vertex, b: Vertex) -> None:
-        self.__vertex
+    def __init__(self, u: Vertex, v: Vertex, w: float = None) -> None:
+        '''An edge "u" -> "v" with cost "w"'''
+        self.__vertex_src = u
+        self.__vertex_dst = v
+        self.__weight = w
+
+    def get_source_vertex(self) -> Vertex:
+        return self.__vertex_src
+    
+    def get_destination_vertex(self) -> Vertex:
+        return self.__vertex_dst
+    
+    def get_weight(self) -> float:
+        return self.__weight
--- tspmodel/graph/Vertex.py
+++ tspmodel/graph/Vertex.py
@@ -1,5 +1,8 @@
 class Vertex:
-    '''An vertex "a" from the graph'''
 
     def __init__(self, id: int) -> None:
+        '''A vertex "u" from the graph'''
         self.__id = id
+    
+    def get_id(self):
+        return self.__id
