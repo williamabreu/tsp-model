8aa3180795eaffe65b0a54dddf53440537c3b3dc
williamabreu <contato@williamabreu.net>
2021-02-15 18:42:11 -0300

    Add hi-level func distance between 2 vertices

--- tspmodel/util/Geometry.py
+++ tspmodel/util/Geometry.py
@@ -1,4 +1,11 @@
+from tspmodel.graph.Vertex import Vertex
+
+
 def distance2d(p0: tuple[float, float], p1: tuple[float, float]) -> float:
     x0, y0 = p0
     x1, y1 = p1
     return ((x1 - x0) ** 2 - (y1 - y0) ** 2) ** 0.5
+
+
+def distance_vertex(u: Vertex, v: Vertex) -> float:
+    return distance2d(u.coordinate(), v.coordinate())
