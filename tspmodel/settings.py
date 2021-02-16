3eb54ca3d2db219bbd16ad942ea607f8c250dcd0
williamabreu <contato@williamabreu.net>
2021-02-16 15:11:07 -0300

    Add global VISUAL_MODE setting

--- tspmodel/settings.py
+++ tspmodel/settings.py
@@ -6,3 +6,9 @@
 # - Is used for indexing and distributing the package
 # - Follow the conventions defined by PEP440
 __version__ = '1.0.dev1'
+
+
+# Enable/disable visual mode for plotting the graph map.
+# - Set True if using Jupyter
+# - Set False if running over huge models from terminal
+VISUAL_MODE = True
