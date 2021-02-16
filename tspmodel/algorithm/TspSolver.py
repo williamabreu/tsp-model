d711712ed698a5b82daaf3a0d45238014f72c443
williamabreu <contato@williamabreu.net>
2021-02-15 22:49:22 -0300

    Add solver base classes

---
+++ tspmodel/algorithm/TspSolver.py
@@ -0,0 +1,25 @@
+from tspmodel.graph.Graph import Graph
+from tspmodel.graph.Vertex import Vertex
+
+
+class TspSolver:
+    def __init__(self, vertices: list[Vertex], graph: Graph) -> None:
+        self.__vertices: list[Vertex] = vertices
+        self.__graph: Graph = graph
+        self.__build_initial_graph()
+
+    def __build_initial_graph(self) -> None:
+        for vertex in self.__vertices:
+            self.__graph.add_vertex(vertex)
+    
+    def graph(self) -> Graph:
+        return self.__graph
+    
+    def vertices(self) -> list[Vertex]:
+        return self.__vertices
+    
+    def solution(self) -> float:
+        pass
+
+    def solve(self, src_vertex: Vertex) -> "TspSolver":
+        pass
---
+++ tspmodel/algorithm/TspSolverGreedy.py
@@ -0,0 +1,32 @@
+from tspmodel.graph.DirectedGraph import DirectedGraph
+from tspmodel.graph.Vertex import Vertex
+from tspmodel.graph.Edge import Edge
+from tspmodel.algorithm.TspSolver import TspSolver
+from tspmodel.util.Geometry import distance_vertex
+
+
+class TspSolverGreedy(TspSolver):
+    def __init__(self, vertices: list[Vertex], visual_mode: bool = False) -> None:
+        super().__init__(vertices, DirectedGraph(visual_mode))
+        self.__solution: float = 0.0
+    
+    def solution(self) -> float:
+        return self.__solution
+
+    def solve(self, src_vertex: Vertex) -> TspSolver:
+        graph = self.graph()
+        vertices = self.vertices()
+        next = src_vertex
+
+        for v in vertices:
+            if v != src_vertex:
+                distance = distance_vertex(next, v)
+                graph.add_edge(Edge(next, v))
+                next = v
+                self.__solution += distance
+        
+        distance = distance_vertex(next, src_vertex)
+        graph.add_edge(Edge(next, src_vertex))
+        self.__solution += distance
+
+        return self
