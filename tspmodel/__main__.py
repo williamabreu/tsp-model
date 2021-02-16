0da62bc93646b9e8d7ed2206d88ee7f7ef4a85d7
williamabreu <contato@williamabreu.net>
2021-02-16 15:35:12 -0300

    Add main module to run the solver

--- tspmodel/__main__.py
+++ tspmodel/__main__.py
@@ -1,5 +1,14 @@
+from tspmodel.iohandler.ConsoleOutput import ConsoleOutput
+from tspmodel.iohandler.ConsoleInput import ConsoleInput
+from tspmodel.algorithm.TspSolverGreedy import TspSolverGreedy
+
+
 def main():
-    print('Hello world!')
+    vertices = ConsoleInput().read()
+    tsp = TspSolverGreedy(vertices)
+    solution = tsp.solve(vertices[0]).solution()
+    ConsoleOutput(f'{solution:.2f}').print()
+
 
 if __name__ == '__main__':
     main()
