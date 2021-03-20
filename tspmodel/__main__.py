449d808162a64f1b6caed13a495f5a327a00baca
williamabreu <contato@williamabreu.net>
2021-03-20 11:42:26 -0300

    Setup console main script

--- tspmodel/__main__.py
+++ tspmodel/__main__.py
@@ -1,14 +1,16 @@
 from tspmodel.iohandler.ConsoleOutput import ConsoleOutput
 from tspmodel.iohandler.ConsoleInput import ConsoleInput
-from tspmodel.algorithm.TspSolverSequential import TspSolverSequential
+from tspmodel.algorithm.TspSolverParticular import TspSolverParticular
+from tspmodel.settings import VISUAL_MODE
 
 
 def main():
     vertices = ConsoleInput().read()
-    tsp = TspSolverSequential(vertices)
+    tsp = TspSolverParticular(vertices)
     solution = tsp.solve().solution()
     ConsoleOutput(f'{solution:.2f}').print()
 
 
 if __name__ == '__main__':
+    VISUAL_MODE = False
     main()
