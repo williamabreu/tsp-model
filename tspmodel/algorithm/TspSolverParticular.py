aac11002f31764385757d72902b87531854727dc
williamabreu <contato@williamabreu.net>
2021-03-21 12:29:18 -0300

    First feasible algorithm version
    Found feasible solution, but not always will be the optimal.

--- tspmodel/algorithm/TspSolverParticular.py
+++ tspmodel/algorithm/TspSolverParticular.py
@@ -15,8 +15,10 @@ class TspSolverParticular(TspSolver):
         super().__init__(vertices, UndirectedGraph())
         self.__solution = float('inf')
         self.__complete_graph: Graph = UndirectedGraph()
+        self.__edge_stack: list[Edge] = []
         self.__further_west: Vertex = None
         self.__further_east: Vertex = None
+        self.__remaining_vertices: int = len(vertices)
         self.__is_right_direction: FunctionType = lambda x0, x1: x0 <= x1
         self.__is_left_direction: FunctionType = lambda x0, x1: x0 >= x1
         self.__build_complete_graph()
@@ -42,46 +44,31 @@ class TspSolverParticular(TspSolver):
     def __sorted_vertices_by_xaxis(self, vertices: list[Vertex]) -> list[Vertex]:
         return sorted(vertices, key=lambda x: x.coordinate())
 
-    # def __get_next_vertex(self, current: Vertex, excluded: Vertex, predicate: FunctionType) -> Vertex:
-    #     neighbors = self.__complete_graph.neighborhood(current)
-    #     min = Vertex(-1).set_cost(float('inf'))
-    #     x0 = current.coordinate()[0]
+    def __is_crossed_edge(self, candidate: Edge,):
+        for edge in self.__edge_stack[1:]:
+            line0 = (edge.source_vertex().coordinate(), edge.destination_vertex().coordinate()) # Line segment edge list
+            line1 = (candidate.source_vertex().coordinate(), candidate.destination_vertex().coordinate()) # Line segment candidate edge
+            if intersects(line0, line1):
+                return True
+        return False
 
-    #     for vertex in neighbors:
-    #         # Check if the vertex...
-    #         #  - was not added to the solution
-    #         #  - is not the excluded from search (further west or east)
-    #         #  - has the coordinate following the direction (right or left)
-    #         #  - is the closest
-    #         #  - TODO: check if it do not make a crossing edge
-
-    #         x1 = vertex.coordinate()[0]
-
-    #         if not vertex[ADDED_KEY] and vertex != excluded and predicate(x0, x1) and vertex.cost() < min.cost():
-    #             min = vertex
-
-    #     return min if min.cost() != float('inf') else None
-
-    def __is_crossed_edge(self, higher_vertex: Vertex, lower_vertex: Vertex, edge: Edge):
-        if higher_vertex.id() < 0 or lower_vertex.id() < 0:
-            return False
-        else:
-            line0 = (higher_vertex.coordinate(), lower_vertex.coordinate()) # Line segment higher_vertex -> lower_vertex
-            line1 = (edge.source_vertex().coordinate(), edge.destination_vertex().coordinate()) # Line segment candidate edge
-            return intersects(line0, line1)
-
-    def __find_path(self, before: Vertex, current: Vertex, higher_vertex: Vertex, lower_vertex: Vertex, is_keeping_direction: FunctionType) -> float:
-        precondition = higher_vertex not in (current, before) and lower_vertex not in (current, before)
+    def __find_path(self, before: Vertex, current: Vertex, is_keeping_direction: FunctionType) -> float:
         current[ADDED_KEY] = True
+        self.__remaining_vertices -= 1
+        graph = self.graph()
 
-        if precondition and self.__is_crossed_edge(higher_vertex, lower_vertex, Edge(before, current)):
+        if current != self.__further_west and self.__is_crossed_edge(Edge(before, current)):
             current[ADDED_KEY] = False
+            self.__remaining_vertices += 1
             return float('inf')
-        elif current == self.__further_west:
+        elif self.__remaining_vertices == 0 and current == self.__further_west:
+            # found the solution!
             w = distance_vertex(current, before)
-            self.graph().add_edge(Edge(before, current, w))
+            graph.add_edge(Edge(before, current, w))
             return w
         else:
+            self.__edge_stack.insert(0, Edge(before, current))
+
             if current == self.__further_east:
                 predicate = self.__is_left_direction # change the direction
             else:
@@ -92,66 +79,16 @@ class TspSolverParticular(TspSolver):
                 x1 = next.coordinate()[0]
 
                 if not next[ADDED_KEY] and predicate(x0, x1):
-                    higher, lower = self.__update_higher_lower(higher_vertex, lower_vertex, current, next)
-                    cost = self.__find_path(current, next, higher, lower, predicate)
+                    cost = self.__find_path(current, next, predicate)
                     if cost != float('inf'):
                         w = distance_vertex(current, before)
-                        self.graph().add_edge(Edge(before, current, w))
+                        graph.add_edge(Edge(before, current, w))
                         return w + cost
 
             current[ADDED_KEY] = False
+            self.__remaining_vertices += 1
+            self.__edge_stack.pop(0)
             return float('inf')
-
-    # def __find_subpath(self, current: Vertex, further: Vertex, predicate: FunctionType, graph: Graph) -> None:
-    #     cost = 0.0
-    #     current[ADDED_KEY] = True
-    #     next = self.__get_next_vertex(current, further, predicate)
-
-    #     while next:
-    #         edge_cost = next.cost()
-    #         cost += edge_cost
-    #         graph.add_edge(Edge(current, next, edge_cost))
-    #         current = next
-    #         current[ADDED_KEY] = True
-    #         next = self.__get_next_vertex(current, further, predicate)
-
-    #     edge_cost = distance_vertex(current, further)
-    #     cost += edge_cost
-    #     graph.add_edge(Edge(current, further, edge_cost))
-
-    #     return cost
-
-    def __get_higher_lower(self, u: Vertex, v: Vertex) -> tuple[Vertex, Vertex]:
-        if u.coordinate()[1] > v.coordinate()[1]:
-            higher = u
-            lower = v
-        else:
-            higher = v
-            lower = u
-        return higher, lower
-
-    def __update_higher_lower(self, higher_vertex: Vertex, lower_vertex: Vertex, u: Vertex, v: Vertex) -> tuple[Vertex, Vertex]:
-        u_y = u.coordinate()[1]
-        v_y = v.coordinate()[1]
-        higher_y = higher_vertex.coordinate()[1]
-        lower_y = lower_vertex.coordinate()[1]
-        higher = higher_vertex
-        lower = lower_vertex
-
-        if u_y > higher_y:
-            higher = u
-            higher_y = u_y
-        if v_y > higher_y:
-            higher = v
-            higher_y = v_y
-        if u_y < lower_y:
-            lower = u
-            lower_y = u_y
-        if v_y < lower_y:
-            lower = v
-            lower_y = v_y
-
-        return higher, lower
 
     def solution(self) -> float:
         return self.__solution
@@ -160,14 +97,10 @@ class TspSolverParticular(TspSolver):
         current = self.__further_west
 
         for next in self.__complete_graph.neighborhood(current):
-            higher, lower = self.__get_higher_lower(current, next)
-            cost = self.__find_path(current, next, higher, lower, self.__is_right_direction)
+            cost = self.__find_path(current, next, self.__is_right_direction)
 
             if cost != float('inf'):
                 self.__solution = cost
                 break
-
-        # self.__solution += self.__find_subpath(further_west, further_east, is_right_direction, final_graph)
-        # self.__solution += self.__find_subpath(further_east, further_west, is_left_direction, final_graph)
 
         return self
